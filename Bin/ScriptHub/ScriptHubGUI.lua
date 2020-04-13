ab576732e835229b5c24f1272b919db986448e0b
ProjectHex <62734043+ProjectHex@users.noreply.github.com>
2020-04-13 14:57:40 +0700

    Add files via upload

---
+++ Bin/ScriptHub/ScriptHubGUI.lua
@@ -0,0 +1,17 @@
+--[[
+	Discord: SHYRPMf
+	1337.xyz#6508
+	Supported games:
+	Boxing Simulator
+	BLOODFEST
+	Arsenal
+	Gym Realms
+	Thick Legends
+	Muscle Legends
+	Egg Clicker
+	Legends Of Speed
+	Saber Simulator
+	Giant Survival!
+]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/1337xyz/robloxscripts/master/ScriptHub.lua"))()
